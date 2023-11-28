e49bcb8be16134c31fb42a5fcc911ff49215726f
Dh H <hdhye89@gmail.com>
2023-11-28 20:50:23 +0900

    문자열 뒤집기

--- src/self/문자열뒤집기.java
+++ src/self/문자열뒤집기.java
@@ -5,16 +5,19 @@ public class 문자열뒤집기 {
 
     public static void main(String[] args) {
 
-        String str = "가나다라마바사아자차카타파하";
-        String reverse = "";
+        // 공백 제거 후 문자열 뒤집기
+        String str = "가나다라 마바사 - 아자차-카 타파%하";
+        String str2 = str.replaceAll("[-%\\s]+", "");
 
-        for (int i = str.length() - 1 ; i >= 0; i--) {
+        // 스트링빌더로 사용해보기
+        StringBuilder answer = new StringBuilder(str2).reverse();
+        answer.toString();
 
-            char c = str.charAt(i);
-            reverse += c;
+//        for (int i = str2.length() - 1 ; i >= 0; i--) {
+//            char c = str2.charAt(i);
+//            answer += c;
 
-        }
+        System.out.println("reverse = " + answer);
 
-        System.out.println("reverse = " + reverse);
     }
 }
