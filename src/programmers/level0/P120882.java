987beb1c459fd9b7412ad5d7d7adf749973aeaab
Dh H <hdhye89@gmail.com>
2023-12-02 17:09:42 +0900

    등수 매기기 푸는 중

--- src/programmers/level0/P120882.java
+++ src/programmers/level0/P120882.java
@@ -2,51 +2,35 @@ package programmers.level0;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 // 등수매기기 https://school.programmers.co.kr/learn/courses/30/lessons/120882
 public class P120882 {
 
     public int[] solution(int[][] score) {
         int[] answer = new int[score.length];
-        answer[0] = 1;
 
         // 인덱스, 평균점수
-        List<Map.Entry<Integer, Integer>> avgList = new ArrayList<>();
-
         Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < score.length;  i++){
-
+        for (int i = 0; i < score.length; i++) {
             int sum = 0;
 
-            for(int j = 0; j < score[i].length; j++){
-
+            for (int j = 0; j < score[i].length; j++) {
                 sum += score[i][j];
-
             }
-            answer[i] = sum/2;
-            map.put(i, sum/2); // 중복 업데이트로 마지막 인덱스만 값에 담김
+
+            int average = sum / score[i].length; // Calculate the average correctly
+            answer[i] = average;
+            map.put(i, average);
 
             System.out.print(" ," + answer[i]);
         }
 
         System.out.println();
+
         int rank = score.length;
-
-        avgList = map.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toList());
-
-        System.out.println("avgList = " + avgList);
-
-        for (int i = 0; i < avgList.size(); i++) {
-
-//            answer[i] = avgList.get(i)
-        }
-//
-//        for (int i = 0; i < answerlist.size(); i++) {
-//            answer[map.get(answerlist.get(i))] = rank;
-//
-//            rank--;
-//        }
+        Stream<Map.Entry<Integer, Integer>> entryMap = map.entrySet().stream().sorted(Map.Entry.comparingByValue());
 
         return answer;
     }
