873479713441cade33d0268bb74bb11a5f2b88cc
다혜 <121746804+HDhye@users.noreply.github.com>
2023-12-07 23:04:00 +0900

    [level unrated] Title: 정사각형으로 만들기, Time: 0.14 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[][] solution(int[][] arr) {
+        int[][] answer;
+        int max = Math.max(arr.length, arr[0].length);
+        
+        answer = new int[max][max];
+        for(int i =0; i < arr.length; i++){
+            answer[i] = Arrays.copyOf(arr[i], max);
+        }
+        return answer;
+    }
+}
