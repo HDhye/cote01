ec58668f48b9634713bfb74e9b7c5b2c6529d257
다혜 <121746804+HDhye@users.noreply.github.com>
2023-12-08 10:23:14 +0900

    [level unrated] Title: 주사위의 개수, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.java
@@ -2,24 +2,9 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] box, int n) {
-        int answer = 1;
-        int dice = (int) Math.pow(n, 3);
+        int answer =1; 
+        answer = (box[0]/n) * (box[1]/n) * (box[2]/n) ;
         
-        for(int b : box){
-            answer *= diceCheck(b, n);
-        }
-           
         return answer;
-    }
-    
-    static private int diceCheck(int boxlen, int n){
-        int count = 0; 
-        while(n <= boxlen){
-            boxlen -= n; 
-            count++; 
-        }
-        System.out.println(count);
-
-        return count;
     }
 }
